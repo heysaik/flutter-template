227043fca66fea2e5245bb30a2e43ae25148720d
Sai Kambampati <ssv1065@gmail.com>
2021-01-24 20:32:43 -0800

    Add files via upload

---
+++ flutter_template/android/app/src/main/kotlin/app/skyhighlabs/flutter_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.skyhighlabs.flutter_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_template/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // START ADDING YOUR WIDGETS HERE
+      ),
+    );
+  }
+}
